897ba59ee7d29e1e3d05de88fbe07f23625faef7
ediogama <ediogama@hotmail.com>
2023-09-23 16:24:21 -0300

    add constructor and validate name

--- src/@core/common/domain/value-objects/name.vo.ts
+++ src/@core/common/domain/value-objects/name.vo.ts
@@ -1,3 +1,12 @@
 import { ValueObject } from './value-object';
 
-export class Name extends ValueObject<string> {}
+export class Name extends ValueObject<string> {
+  constructor(name: string) {
+    super(name);
+    this.isValid();
+  }
+
+  isValid() {
+    return this.value.length >= 3;
+  }
+}
