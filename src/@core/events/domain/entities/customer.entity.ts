3b487d0bc8ee0a7c566d9b56b951b26278287690
ediogama <ediogama@hotmail.com>
2023-09-23 16:22:38 -0300

    add Name in name

--- src/@core/events/domain/entities/customer.entity.ts
+++ src/@core/events/domain/entities/customer.entity.ts
@@ -1,15 +1,16 @@
 import { AggregateRoot } from 'src/@core/common/domain/aggregate-root';
+import { Name } from 'src/@core/common/domain/value-objects/name.vo';
 
 export type CustomerConstructorProps = {
   id?: string;
   cpf: string;
-  name: string;
+  name: Name;
 };
 
 export class Customer extends AggregateRoot {
   id: string;
   cpf: string;
-  name: string;
+  name: Name;
 
   constructor(props: CustomerConstructorProps) {
     super();
