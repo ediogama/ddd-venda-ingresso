b7a501f661b145f4057b869a07ff6c0905cd7cd1
ediogama <ediogama@hotmail.com>
2023-09-24 10:16:40 -0300

    alter imports

--- src/@core/events/domain/entities/customer.entity.ts
+++ src/@core/events/domain/entities/customer.entity.ts
@@ -1,6 +1,6 @@
-import { AggregateRoot } from '../../../../../src/@core/common/domain/aggregate-root';
-import Cpf from 'src/@core/common/domain/value-objects/cpf.vo';
-import { Name } from 'src/@core/common/domain/value-objects/name.vo';
+import { AggregateRoot } from './../../../../../src/@core/common/domain/aggregate-root';
+import Cpf from './../../../../../src/@core/common/domain/value-objects/cpf.vo';
+import { Name } from './../../../../../src/@core/common/domain/value-objects/name.vo';
 import Uuid from './../../../../../src/@core/common/domain/value-objects/uuid.vo';
 
 export class CustomerId extends Uuid {}
